d989a3572e6c5da7a15d62fa3f0449f7402a9d0f
Nitish Kumar <nitishkumar@outlook.com>
2023-01-02 23:48:41 +0530

    updated

--- 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.6"
+  required_version = "~> 1.3.6"
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -19,7 +19,7 @@ provider "aws" {
 provider "aws" {
   region = "us-west-1"
   profile = "default"
-  alias = "aws-west-1"
+  alias = "uswest1"
 }
 
 
--- 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c3-vpc-us-west-1.tf
+++ 03-Terraform-Fundamental-Blocks/03-03-Multiple-Provider-Configurations/terraform-manifests/c3-vpc-us-west-1.tf
@@ -1,7 +1,7 @@
 # Resource Block to Create VPC in us-west-1
 resource "aws_vpc" "vpc-us-west-1" {
   cidr_block = "10.1.0.0/16"
-  provider = aws.aws-west-1
+  provider = aws.uswest1
   tags = {
     "Name" = "vpc-us-west-1"
   }
