2d4bfe96528f82f35f634c3e1d1051adffa52518
Nitish Kumar <nitishkumar@outlook.com>
2023-01-07 23:42:44 +0530

    updated toset

--- 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
+++ 04-Terraform-Resources/04-04-Meta-Argument-for_each/v2-for_each-toset/c2-iamuser.tf
@@ -1,7 +1,7 @@
 # Create 4 IAM Users
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user
 
-resource "aws_iam_user" "myuser" {
-  for_each = toset(["TJack", "TJames", "TMadhu", "TDave"])
-  name     = each.key
+resource "aws_iam_user" "myusers" {
+  for_each = toset(["Tjack", "TJames", "TMadhu", "TDave"])
+  name = each.value
 }
