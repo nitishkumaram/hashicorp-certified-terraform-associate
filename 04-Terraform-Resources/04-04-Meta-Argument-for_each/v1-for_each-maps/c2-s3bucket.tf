e69d807683b61d7ac7bf0401dad497e54efd67f3
Nitish Kumar <nitishkumar@outlook.com>
2023-01-07 22:45:14 +0530

    updated meta argument

--- 04-Terraform-Resources/04-04-Meta-Argument-for_each/v1-for_each-maps/c2-s3bucket.tf
+++ 04-Terraform-Resources/04-04-Meta-Argument-for_each/v1-for_each-maps/c2-s3bucket.tf
@@ -2,21 +2,28 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
 resource "aws_s3_bucket" "mys3bucket" {
+  
+  # for_each Meta argument
 
-  # for_each Meta-Argument
   for_each = {
-    dev  = "my-dapp-bucket"
-    qa   = "my-qapp-bucket"
-    stag = "my-sapp-bucket"
-    prod = "my-papp-bucket"
+    "dev"  = "my-dapp-bucketnitish"
+    "qa"   = "my-qapp-bucketnitish"
+    "stag" = "my-sapp-bucketnitish"
+    "prod" = "my-papp-bucketnitish"
   }
-
   bucket = "${each.key}-${each.value}"
   acl    = "private"
 
   tags = {
-    Environment = each.key
+    Environment = "${each.key}"
     bucketname  = "${each.key}-${each.value}"
     eachvalue   = each.value
   }
 }
+
+# resource "aws_s3_bucket_acl" "bucket_acl" {
+#   bucket = aws_s3_bucket.mys3bucket.id
+#   acl    = "private"
+# }
+
+# for_each Meta-Argument
