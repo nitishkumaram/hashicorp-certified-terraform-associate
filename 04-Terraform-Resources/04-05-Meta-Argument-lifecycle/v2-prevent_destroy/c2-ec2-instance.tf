e0f1bc5766472ba646f51a0302d5d58f7cf27034
Nitish Kumar <nitishkumar@outlook.com>
2023-01-11 14:01:48 +0530

    createbeforedestroy

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
@@ -1,12 +1,11 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami = "ami-0b5eea76982371e91" # Amazon Linux
   instance_type = "t2.micro"
   tags = {
     "Name" = "web-2"
   }
   lifecycle {
-    prevent_destroy = true # Default is false
+    create_before_destroy = true
   }
 }
-
